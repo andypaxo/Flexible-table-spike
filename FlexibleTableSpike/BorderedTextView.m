1560a680d32c3c0d091e4905fcc678dd14eba87a
Andy <andy@andypaxo.net>
2012-03-12 20:36:40 -0600

    Showing text view in cell

--- FlexibleTableSpike/BorderedTextView.m
+++ FlexibleTableSpike/BorderedTextView.m
@@ -1,11 +1,3 @@
-//
-//  BorderedTextView.m
-//  FlexibleTableSpike
-//
-//  Created by Andrew Palmowski on 12-03-12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "BorderedTextView.h"
 
 @implementation BorderedTextView
@@ -14,18 +6,18 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        self.font = [UIFont systemFontOfSize:15.0f];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGFloat black[4] = { 0, 0, 0, 1 };
+    CGContextSetFillColor(context, black);
+    
+    CGContextStrokeRect(context, [self bounds]);
 }
-*/
 
 @end
--- FlexibleTableSpike/QuestionCell.m
+++ FlexibleTableSpike/QuestionCell.m
@@ -1,24 +1,16 @@
-//
-//  QuestionCell.m
-//  FlexibleTableSpike
-//
-//  Created by Andrew Palmowski on 12-03-11.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "QuestionCell.h"
 #import "BorderedTextView.h"
 
-#define LABEL_TAG 30
-#define EXPANDED_LABEL_TAG 10
-#define TEXT_BOX_TAG 20
-
-static int padding = 15;
-static int labelWidth = 550;
+static int __padding = 15;
+static int __labelWidth = 550;
+static int __editBoxHeight = 90;
 
 @interface QuestionCell ()
 
 @property (nonatomic) BorderedTextView* notesView;
+
++ (CGFloat) notesTopFor:(Question*) question;
++ (CGFloat) notesHeightFor:(Question*) question;
 
 @end
 
@@ -32,40 +24,23 @@ static int labelWidth = 550;
     if (!_notesView) {
         _notesView = [BorderedTextView new];
         _notesView.text = @"Your notes...";
+        [self addSubview:_notesView];
     }
     return _notesView;
 }
 
 - (void) setQuestion:(Question *)question {
     _question = question;
-    ((UILabel*)[self viewWithTag:LABEL_TAG]).text = question.text;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.3f];
+    self.notesView.alpha = _question.hasNotes;
+    [UIView commitAnimations];    
+    
+    self.notesView.frame = CGRectMake(__padding, [QuestionCell notesTopFor:question],
+                                      __labelWidth, __editBoxHeight);
+    
     [self setNeedsDisplay];
-}
-
-//- (void)setIsExpanded:(BOOL)isExpanded {
-//    _isExpanded = isExpanded;
-//    
-//    if (_question != nil) {
-//        [UIView beginAnimations:nil context:NULL];
-//        [UIView setAnimationDuration:0.25];
-//    }
-//    
-//    [self viewWithTag:EXPANDED_LABEL_TAG].alpha = isExpanded;
-//    [self viewWithTag:TEXT_BOX_TAG].alpha = isExpanded;
-//    
-//    
-//    if (_question != nil) {
-//        [UIView commitAnimations];
-//    }
-//}
-
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
-    return self;
 }
 
 - (void)drawRect:(CGRect)rect {
@@ -73,27 +48,30 @@ static int labelWidth = 550;
     
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGFloat black[4] = { 0, 0, 0, 1 };
-   
     CGContextSetFillColor(context, black);
-    [_question.text drawInRect:CGRectMake(15, 15, labelWidth, 9999) withFont:labelFont];
+    
+    [_question.text drawInRect:CGRectMake(15, 15, __labelWidth, 9999) withFont:labelFont];
 }
 
-+ (CGFloat)heightRequiredBy:(Question *)question {
++ (CGFloat)notesTopFor:(Question *)question {
     UIFont* labelFont = [UIFont systemFontOfSize:17.0f];
     
-    CGSize labelBounds = CGSizeMake(labelWidth, 9999);
+    CGSize labelBounds = CGSizeMake(__labelWidth, 9999);
     
     CGSize actualTextSize = [question.text
                              sizeWithFont:labelFont
                              constrainedToSize:labelBounds
                              lineBreakMode:UILineBreakModeWordWrap];
     
-//    NSLog(@"%dpx required for: %@", (int)actualTextSize.height, question.text);
-    
-    return
-    padding
-    + actualTextSize.height
-    + padding;
+    return 2 * __padding + actualTextSize.height;
+}
+
++ (CGFloat)notesHeightFor:(Question *)question {
+    return (__editBoxHeight + __padding) * question.hasNotes;
+}
+
++ (CGFloat)heightRequiredBy:(Question *)question {    
+    return [self notesTopFor:question] + [self notesHeightFor:question];
 }
 
 @end
